4520db7070b90189c2237e700f3bf71981808b75
Poppy Sari <poppy.sari@rga.com>
2021-10-26 03:06:57 +0700

    :sparkles: feat: stateless to stateful

--- lib/main.dart
+++ lib/main.dart
@@ -15,20 +15,25 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
-  int leftDiceNumber = 5;
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
 
   @override
   Widget build(BuildContext context) {
-    leftDiceNumber = 2;
-
     return Center(
       child: Row(
         children: <Widget>[
           Expanded(
             child: TextButton(
               onPressed: () {
-                print('Left dice rolled');
+                setState(() {
+                  leftDiceNumber = 5;
+                });
               },
               child: Image.asset(
                 'images/dice$leftDiceNumber.png',
